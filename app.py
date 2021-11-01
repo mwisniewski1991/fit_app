8596c61fcb0b050272ef0676c97b32f090770392
mwisniewski1991 <m.wisniewski1991@gmail.com>
2021-11-01 12:39:25 +0100

    Create callback for dash (without update new data to database yet

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from dash import dash, dcc, html, Input, Output
 import plotly.graph_objects as go
-from tabs.weight_tracker import weight_tracker_frame
+from tabs.weight_tracker import weight_tracker_frame, colors, weight_layout
+import data_manager
+
 
 app = dash.Dash(__name__)
 app.layout = html.Div(children=[
@@ -12,6 +14,30 @@ app.layout = html.Div(children=[
 
     ])
 ])
+
+@app.callback(
+    Output(component_id='weight_chart', component_property='figure'),
+    [   Input(component_id='weight_input', component_property='value'),
+        Input(component_id='weight_button', component_property='n_clicks')
+    ]
+)
+def add_new_weight_data(weight, n_clicks):
+
+    if n_clicks != None:
+        print('DATA TO DB')
+        print(weight, n_clicks)
+    else:
+        print('ONLY LOAD')
+        print(weight)
+
+    weight_df = data_manager.import_data()
+    weight_trace = go.Scatter(
+        x=weight_df['report_time'],
+        y=weight_df['weight'],
+        line_color=colors['white']
+    )
+
+    return {'data': [weight_trace], 'layout': weight_layout}
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- tabs/weight_tracker.py
+++ tabs/weight_tracker.py
@@ -2,22 +2,13 @@ from dash import dcc, html
 import plotly.graph_objects as go
 import data_manager
 
-def weight_tracker_frame():
-
-    weight_df = data_manager.import_data()
-
-    colors = {
+colors = {
         'grid_color': 'rgba(0, 0, 0, 0.25)',
         'transparent': 'rgba(0, 0, 0, 0)',
         'white': 'rgb(255,255,255)',
     }
-    
-    weight_trace = go.Scatter(
-        x=weight_df['report_time'],
-        y=weight_df['weight'],
-        line_color=colors['white'])
 
-    weight_layout = {
+weight_layout = {
         'title': {'text': 'Weight tracker', 'color': colors['white']}, 
         'font': {'family':'Georama', 'color': colors['white']},
         'paper_bgcolor': colors['transparent'],
@@ -29,29 +20,54 @@ def weight_tracker_frame():
         'showlegend': False,
     }
 
+def weight_tracker_frame():
+
+    weight_df = data_manager.import_data()
+
+    colors = {
+        'grid_color': 'rgba(0, 0, 0, 0.25)',
+        'transparent': 'rgba(0, 0, 0, 0)',
+        'white': 'rgb(255,255,255)',
+    }
+
+    weight_layout = {
+            'title': {'text': 'Weight tracker', 'color': colors['white']}, 
+            'font': {'family':'Georama', 'color': colors['white']},
+            'paper_bgcolor': colors['transparent'],
+            'plot_bgcolor': colors['transparent'],
+            'xaxis': {'showline':False, 'gridcolor': colors['grid_color'], 'tickformat': '%Y-%m-%d', 'ticklabelmode': 'period'},
+            'yaxis': {'showline':True, 'gridcolor': colors['grid_color']},
+            'margin_b': 10, 'margin_t': 50, 'margin_r': 10,
+            'margin': {'b': 40, 't': 50, 'r': 50, 'l': 40},
+            'showlegend': False,
+        }
+
+    weight_trace = go.Scatter(
+        x=weight_df['report_time'],
+        y=weight_df['weight'],
+        line_color=colors['white'])
 
     wt = html.Div(className='weightTracker', children=[
             html.Div(className='inputs', children=[
                 html.Span(className='inputs__text', children='Put your current weight.'),
-                dcc.Input(
+                dcc.Input(id='weight_input',
                 className='inputs__input',
                 type='number',
                 value=80.2,
                 step=0.1,
                 ),
-                html.Button(className='inputs__button', children='Submit')
+                html.Button(id='weight_button',className='inputs__button', children='Submit')
             ]),
 
             html.Div(className='charts', children=[
-            dcc.Graph(figure={'data':[weight_trace], 'layout': weight_layout }, 
+            dcc.Graph(id='weight_chart',
+                figure={'data':[weight_trace], 'layout': weight_layout }, 
                 config={'displayModeBar': False},
                 # config={'modeBarButtonsToRemove': ['pan2d', 'lasso2d']}
                 )
             ])
         ])
     return wt
-
-
 
 if __name__ == '__main__':
     print(weight_tracker_frame())
